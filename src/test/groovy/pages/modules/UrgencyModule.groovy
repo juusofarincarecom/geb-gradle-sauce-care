294c32b9cdcd230e6dfb2356ff17aaf54ba6d2f4
Juuso Farin <juuso.farin@care.com>
2023-04-07 13:05:37 +0200

    Jenkinsfile fixes

--- src/test/groovy/pages/modules/UrgencyModule.groovy
+++ src/test/groovy/pages/modules/UrgencyModule.groovy
@@ -9,12 +9,12 @@ class UrgencyModule extends BaseModule {
     static content = {
         urgencyModule(wait: "verySlow") { $("div", role: "presentation", class: contains("MuiDialog-root")) }
         rightNow { urgencyModule.$("p", text: "Right now") }
-        closeButton(required: false) { urgencyModule.$("button", class: contains("MuiIconButton-sizeSmall")) }
+        closeButton(required: false) { $("button", class: contains("MuiIconButton-sizeSmall")) }
     }
     
     @Step("Wait for Urgency Module to show")
     def waitForUrgencyModule() {
-        browser.waitFor("verySlow") { urgencyModule.displayed }
+        waitFor("verySlow") { urgencyModule.displayed }
     }
     
     @Step("Click Urgency Module 'Right Now' Button")
@@ -29,15 +29,8 @@ class UrgencyModule extends BaseModule {
     
     @Step("Wait For Urgency Module to not be displayed")
     def closeButtonNotDisplayed() {
-        sleep(2000)
-        //log.info("closeButton.displayed: " + closeButton.displayed.toString())
-        log.info("closeButton.isDisplayed(): " + closeButton.isDisplayed())
         log.info("closeButton.isEmpty(): " + closeButton.isEmpty())
-        log.info("closeButton.isFocused(): " + closeButton.isFocused())
-        log.info("closeButton.value(): " + closeButton.value())
-        log.info("closeButton.allElements(): " + closeButton.allElements())
-        log.info("closeButton.tag(): " + closeButton.tag())
-        
-        assert !closeButton.displayed
+        log.info("urgencyModule.isEmpty(): " + urgencyModule.isEmpty())
+        waitFor("verySlow") { urgencyModule.isEmpty() }
     }
 }
